f713cae31c2e023f5e1da591d07a4472f7eed411
Aneeq web developer <aneeq.hassan2006@gmail.com>
2026-09-22 08:14:17 -0700

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="site-footer">
-      <div className="container">
-        <p>&copy; 2026 XYZ Dental Clinic. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
